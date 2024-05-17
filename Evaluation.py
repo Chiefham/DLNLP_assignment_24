95b555b2ccf5f77c8a1d9a544e1b10ed48fa953e
WooChickham <1580103639@qq.com>
2024-05-17 18:38:29 +0800

    second commit

---
+++ Evaluation.py
@@ -0,0 +1,33 @@
+import config
+import pickle
+import numpy as np
+from tqdm import tqdm
+from keras_bert import get_custom_objects
+from keras.models import load_model
+from keras_bert import load_vocabulary, Tokenizer
+from sklearn.metrics import hamming_loss, classification_report
+from utils import load_data,predict_single_text
+
+
+def evaluate(mlb,model,tokenizer):
+    test_x, test_y = load_data(config.test_dataset_path)
+    true_y_list = mlb.transform(test_y)
+
+    pred_y_list = []
+    pred_labels = []
+    for text in tqdm(test_x):
+        pred_y, label = predict_single_text(text,model,tokenizer,mlb)
+        pred_y_list.append(pred_y)
+        pred_labels.append(label)
+
+    # 计算accuracy，一条数据的所有标签全部预测正确则1，否则为0。
+    test_len = len(test_y)
+    correct_count = 0
+    for i in range(test_len):
+        if test_y[i] == pred_labels[i]:
+            correct_count += 1
+    accuracy = correct_count / test_len
+
+    print(classification_report(true_y_list, pred_y_list, target_names=mlb.classes_.tolist(), digits=4))
+    print("accuracy:{}".format(accuracy))
+    print("hamming_loss:{}".format(hamming_loss(true_y_list, pred_y_list)))
